408b9bdaffcf873fdd51563eed467a00af839d67
Andrew Zimmerman <astevezimm@gmail.com>
2024-09-14 16:05:55 -0600

    Implement KirbySlideShow other than using correct images

--- app/routes/_app.about-me/KirbySlideShow.tsx
+++ app/routes/_app.about-me/KirbySlideShow.tsx
@@ -1,3 +1,33 @@
+import {useEffect, useState} from 'react'
+
+const LIST = [
+  "images/byu.jpg",
+  "images/hiking.jpg",
+  "images/kirby.jpg",
+  "images/mission.jpg",
+]
+
 export default function KirbySlideShow() {
-  return null;
+  const [index, setIndex] = useState(0)
+  const [className, setClassName] = useState('')
+
+  useEffect(() => {
+    if (!className.includes('leave')) return
+    const prev = () => setIndex(index => (index + LIST.length - 1) % LIST.length)
+    const next = () => setIndex(index => (index + 1) % LIST.length)
+    const timeout = setTimeout(() => {
+      setClassName('')
+      if (className.includes('left')) prev()
+      else if (className.includes('right')) next()
+    }, 500)
+    return () => clearTimeout(timeout);
+  }, [className]);
+  
+  return (
+    <div className="slideshow">
+      <button onClick={() => setClassName('leave left')}>&lt;</button>
+      <img src={LIST[index]} className={className} alt="Kirby"/>
+      <button onClick={() => setClassName('leave right')}>&gt;</button>
+    </div>
+  )
 }
