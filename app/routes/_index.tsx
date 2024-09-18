f21a613e39fea2c405634c14a0422642b82fd693
Andrew Zimmerman <astevezimm@gmail.com>
2024-09-18 13:54:36 -0600

    Fix meta for _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,9 @@
 import { useEffect } from "react"
 import { useNavigate } from "@remix-run/react"
+import type {MetaFunction} from '@remix-run/node'
+import {makeMeta} from '~/global'
+
+export const meta: MetaFunction = () => makeMeta("", "Welcome to the website for Andrew Zimmerman!")
 
 export default function HomePage() {
   const navigate = useNavigate()
